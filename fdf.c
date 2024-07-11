4b2f9ec09c576da1b8706948c8e74cb708b2ec03
doomangang <sp0943@cau.ac.kr>
2024-07-11 16:28:14 +0900

    handle func norm

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: jihyjeon <jihyjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 18:06:12 by jihyjeon          #+#    #+#             */
-/*   Updated: 2024/07/11 15:56:45 by jihyjeon         ###   ########.fr       */
+/*   Updated: 2024/07/11 16:27:50 by jihyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,9 +98,8 @@ void	set_them_free(char **arr)
 	arr = 0;
 }
 
-void	handle_empty()
+void	handle_empty(void)
 {
 	fprintf(stderr, "Error: empty map\n");
-    exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
-
